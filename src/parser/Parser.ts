9bd8869b20bc8eed73b9d002896f89f09e964fac
Philip Flyvholm <philipflyvholm@gmail.com>
2022-09-29 16:57:58 +0200

    Line number fix

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -6,7 +6,7 @@ import Attribute from './Nodes/Attribute.js';
 import { error, ParseError } from './Utils/ParseError.js';
 import Text from './Nodes/Text.js';
 import { parse as acornParse } from 'acorn';
-
+import os from 'os';
 export default class Parser {
     private lexer: Lexer;
     private line = {
@@ -26,7 +26,7 @@ export default class Parser {
             switch (type) {
                 case LexTypes.Newline:
                     this.line = {
-                        number: this.line.number + 1,
+                        number: template.substring(0, token.end).split(/\r\n|\r|\n/).length,
                         startIndex: token.end
                     }
                     break;
@@ -150,6 +150,8 @@ export default class Parser {
 
 
     printStack = () => {
+        console.log("print line", JSON.stringify(os.EOL));
+        
         console.log(this.stack);
     }
 }
